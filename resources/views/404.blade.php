966d0503250da079bc006e33c3530fc392385559
YASINRA <yas.am.1390@gmail.com>
2024-05-31 18:50:19 +0330

    Fallback Route

---
+++ resources/views/404.blade.php
@@ -0,0 +1,5 @@
+<h2>404 Page</h2>
+
+<p>
+    This is a custom 404 page
+</p>
--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name('frontend_home');
+// Route::get('/', function () {
+//     return view('home');
+// })->name('frontend_home');
 
+
+// Route::get('about', function () {
+//     return view('about');
+// })->name('frontend_about');
 
 Route::get('about', function () {
-    return view('about');
-})->name('frontend_about');
+    return "About Us";
+});
+
+Route::get('/', function () {
+    return "Home page";
+});
+
+Route::fallback(function() {
+    return view('404');
+});
 
 
 Route::get('/about/{username}/{age}', function ($username, $age) {
     //return view('about', ['username' => $username, 'age' => $age]);
     return view('about', compact('username', 'age'));
 });
-
-/*
-Route::post()
-Route::put()
-Route::delete()
-*/
