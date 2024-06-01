d3c0abd7644653fc5c18c649f367f3daea6b7b4e
YASINRA <yas.am.1390@gmail.com>
2024-06-01 15:10:48 +0330

    Route Group

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,17 @@ Route::fallback(function() {
     return view('404');
 });
 
+Route::middleware(['auth', 'admin'])->group(function() {
+    Route::get('dashboard', function () {return "Dashboard";});
+    Route::get('profile', function () {return "Profile";});
+    Route::get('photo', function () {return "Photo";});
+});
+
+Route::prefix('admin')->group(function() {
+    Route::get('about', function () {return "About";});
+    Route::get('contact', function () {return "Contact";});
+    Route::get('settings', function () {return "Settings";});
+});
 
 Route::get('/about/{username}/{age}', function ($username, $age) {
     //return view('about', ['username' => $username, 'age' => $age]);
