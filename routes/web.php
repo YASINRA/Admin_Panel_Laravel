ea8494bc6c4c0dfeef7a2ebbba63eee46852be11
YASINRA <yas.am.1390@gmail.com>
2024-06-01 15:15:27 +0330

    Route Redirect

--- routes/web.php
+++ routes/web.php
@@ -2,40 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('home');
-// })->name('frontend_home');
-
-
-// Route::get('about', function () {
-//     return view('about');
-// })->name('frontend_about');
-
-Route::get('about', function () {
-    return "About Us";
+Route::get('/', function (){
+    return view('welcome');
 });
 
-Route::get('/', function () {
-    return "Home page";
+Route::get('about', function (){
+    return "About Page";
 });
 
-Route::fallback(function() {
-    return view('404');
+Route::get('contact', function (){
+    return "Contact Page";
 });
 
-Route::middleware(['auth', 'admin'])->group(function() {
-    Route::get('dashboard', function () {return "Dashboard";});
-    Route::get('profile', function () {return "Profile";});
-    Route::get('photo', function () {return "Photo";});
-});
-
-Route::prefix('admin')->group(function() {
-    Route::get('about', function () {return "About";});
-    Route::get('contact', function () {return "Contact";});
-    Route::get('settings', function () {return "Settings";});
-});
-
-Route::get('/about/{username}/{age}', function ($username, $age) {
-    //return view('about', ['username' => $username, 'age' => $age]);
-    return view('about', compact('username', 'age'));
-});
+//Route::redirect('about', 'contact', 301);
+Route::permanentRedirect('about', 'contact');
