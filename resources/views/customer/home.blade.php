cf45f05ecf4922d35a095d77647161566ec4396c
YASINRA <yas.am.1390@gmail.com>
2024-06-11 19:34:36 +0330

    Nested View Directories

---
+++ resources/views/customer/home.blade.php
@@ -0,0 +1,1 @@
+<h1>Customer Home Page</h1>
--- routes/web.php
+++ routes/web.php
@@ -6,30 +6,10 @@ Route::get('/', function (){
     return view('welcome');
 });
 
-// Route::resource('articles', 'ArticlesController');
-
-// Route::get('/articles', [ArticlesController::class, 'index'])->name('articles.index');
-// Route::get('/articles/create', [ArticlesController::class, 'create'])->name('articles.create');
-// Route::post('/articles/store', [ArticlesController::class, 'store'])->name('articles.store');
-// Route::get('/articles/{article}', [ArticlesController::class, 'show'])->name('articles.show');
-// Route::get('/articles/{article}/edit', [ArticlesController::class, 'edit'])->name('articles.edit');
-// Route::put('/articles/{article}', [ArticlesController::class, 'update'])->name('articles.update');
-// Route::patch('/articles/{article}', [ArticlesController::class, 'update']);
-// Route::delete('/articles/{article}', [ArticlesController::class, 'destroy'])->name('articles.destroy');
-
-Route::get('about/{id}', function ($id) {
-    return "About Page";
-})->where('id', '[0-9]+');
-
-Route::get('student/{username}', function ($username) {
-    //return "About Page";
-})->where('username', '[a-zA-Z0-9]+');
-
-
-Route::get('student/{id}', function ($id){
-    //
+Route::get('admin/home', function (){
+    return view('admin.home');
 });
 
-Route::get('teacher/{id}', function ($id){
-    //
+Route::get('customer/home', function (){
+    return view('customer.home');
 });
