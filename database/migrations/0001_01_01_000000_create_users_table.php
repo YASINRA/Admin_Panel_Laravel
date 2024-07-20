8cea9d59b9b0abc1f5cc3385a6b0d98fec5ab41c
YASINRA <yas.am.1390@gmail.com>
2024-07-20 18:40:01 +0330

    Update User Table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'customer', 'vendor'])->default('customer');
+            $table->boolean('status')->default(0); // 0 for inactive
             $table->rememberToken();
             $table->timestamps();
         });
