316460044656683cc5dcfd1fdf2873af5b38b448
YASINRA <yas.am.1390@gmail.com>
2024-07-21 19:19:53 +0330

    Creating Controllers Views

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminPanel\AdminController;
+use App\Http\Controllers\VendorPanel\VendorController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +20,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('admin/dashboard', [AdminController::class, 'index']);
+    Route::get('vendor/dashboard', [VendorController::class, 'index']);
+});
